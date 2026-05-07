947af7dcaae9a068eae020fb03602ed7b89171e8
Edsonjr1911 <edsonjr1911@hotmail.com>
2026-05-06 22:24:11 -0300

    Add files via upload
    aula0605

---
+++ operadores logicos.py
@@ -0,0 +1,17 @@
+# idade = int(input("Digite sua idade") )
+# cnh = input("Tem CNH? (sim/não): ")
+# if idade >= 18 and cnh == "sim":
+#     print("Voce pode dirigir")
+# else:
+#     print (" Não Pode")
+
+# controlador logico and
+
+idoso = (input(" Você é idoso? (sim/não)"))
+estudante = (input("você é estudante? (sim/não)"))
+if idoso == "sim" or estudante == "sim":
+    print ("Desconto") 
+else:
+    print ("Paga inteira!")
+    
+    
